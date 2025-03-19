7342e0b5f2298701a3716e4b2710f9c3157d530f
Remco Stoeten <remcostoeten@hotmail.com>
2025-03-19 16:19:35 +0100

    feat: update UI components and add new component files

---
+++ src/shared/components/core/index.ts
@@ -0,0 +1,4 @@
+export { CoreButton } from './core-button';
+export { ButtonVariant } from './core-button';
+export { ButtonSize } from './core-button';
+export { CoreButtonProps } from './core-button';
--- src/shared/components/ui/slider.tsx
+++ src/shared/components/ui/slider.tsx
@@ -18,9 +18,9 @@ const Slider = React.forwardRef<
         {...props}
     >
         <SliderPrimitive.Track
-            className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-secondary"
+            className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-red"
         >
-            <SliderPrimitive.Range className="absolute h-full bg-primary" />
+            <SliderPrimitive.Range className="absolute h-full bg-background" />
         </SliderPrimitive.Track>
         <SliderPrimitive.Thumb
             className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
