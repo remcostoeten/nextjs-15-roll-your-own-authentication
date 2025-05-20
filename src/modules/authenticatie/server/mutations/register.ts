a3b41980dc2e290aaf38dd7f3320b55feb77010a
Remco Stoeten <remcostoeten@hotmail.com>
2025-05-20 10:08:28 +0200

    chore: update avatar gif urls
    Updates Tenor GIF URLs used for default avatar options in the user registration mutation.
    
    Commit generated by VibeGit

--- src/modules/authenticatie/server/mutations/register.ts
+++ src/modules/authenticatie/server/mutations/register.ts
@@ -38,12 +38,12 @@ export async function register(formData: FormData): Promise<RegisterResult> {
 		const AVATAR_OPTIONS = {
 			FAT_MAJIN_BUU:
 				'https://pa1.narvii.com/5858/8b61faaa49264942a3cf812503cccab83fe515a8_hq.gif',
-			FAT_MAJIN_BUU_2: 'https://c.tenor.com/d9GvSsPKxe8AAAAM/majin-buu-buu.gif',
+			FAT_MAJIN_BUU_2: 'https://c.tenor.com/d9GvSsPKxe8M/majin-buu-buu.gif',
 			FAT_MAJIN_BUU_3:
 				'https://www.laguiadelvaron.com/wp-content/uploads/2018/03/majin_bu_dog.gif',
-			FAT_MAJIN_BUU_4: 'https://c.tenor.com/xHMtksLVNlcAAAAC/dbz-gif.gif',
-			FAT_MAJIN_BUU_5: 'https://media.tenor.com/z5Y_hp08qAEAAAAC/buu-tongue.gif',
-			FAT_MAJIN_BUU_6: 'https://c.tenor.com/K3AE3cWEj3QAAAAM/eating-buu.gif',
+			FAT_MAJIN_BUU_4: 'https://c.tenor.com/xHMtksLVNlcC/dbz-gif.gif',
+			FAT_MAJIN_BUU_5: 'https://media.tenor.com/z5Y_hp08qAEC/buu-tongue.gif',
+			FAT_MAJIN_BUU_6: 'https://c.tenor.com/K3AE3cWEj3QM/eating-buu.gif',
 		};
 
 		const isAdmin = email === env.ADMIN_EMAIL;
