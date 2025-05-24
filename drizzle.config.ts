2e04626ee86ed23791101cd806ae8a4525956a71
Remco Stoeten <remcostoeten@hotmail.com>
2025-05-24 14:52:17 +0200

    chore: update docker-compose and add new components

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'drizzle-kit';
-import { env } from 'env';
+  import { defineConfig } from 'drizzle-kit';
+  import { env } from 'env';
 
-export default defineConfig({
-  out: './drizzle',
-  schema: './src/api/db/schema.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-});
+  export default defineConfig({
+    out: './drizzle',
+    schema: './src/api/db/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: env.DATABASE_URL,
+    },
+  });
